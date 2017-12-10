80c2307e0d78725d4a1a4b01f032c2329240272e
Mozillex <Mozillex@users.noreply.github.com>
2017-12-10 00:34:40 -0500

    Inititing directory. 2 files created.

---
+++ script.js
@@ -0,0 +1,12 @@
+
+function createParagraph(){
+  var para = document.createElement('p');
+  para.textContent = "Hey, thanks!!! That's nice.";
+  document.body.appendChild(para);
+}
+
+var buttons = document.querySelectorAll('button');
+
+for (var i=0;i<buttons.length;i++){
+  buttons[i].addEventListener('click', createParagraph);
+}
