67fa105afc0d11007f302c4c1d06daf74a21b609
Mozillex <Mozillex@users.noreply.github.com>
2017-12-24 10:57:38 -0500

    added comment

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
-
+// - - create new paragraph on any button click
 function createParagraph(){
-  var para = document.createElement('p');
-  para.textContent = "Hey, thanks!!! That's nice.";
-  document.body.appendChild(para);
+    var para = document.createElement('p');
+    para.textContent = "Hey, thanks!!! That's nice.";
+    document.body.appendChild(para);
 }
 
 var buttons = document.querySelectorAll('button');
 
 for (var i=0;i<buttons.length;i++){
-  buttons[i].addEventListener('click', createParagraph);
+    buttons[i].addEventListener('click', createParagraph);
 }
+
+//- - new section - - 
